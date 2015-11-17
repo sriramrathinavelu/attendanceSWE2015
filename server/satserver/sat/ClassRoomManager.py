c0fd464527522946f7ccc6900a8a590d156fa146
Sriram Rathinavelu <sriramrathinavelu@gmail.com>
2015-11-16 18:38:46 -0800

    Adding class-room-manager

---
+++ server/satserver/sat/ClassRoomManager.py
@@ -0,0 +1,17 @@
+import django
+django.setup()
+
+
+from models import *
+
+def addClassRoom(code, name, center, radius):
+	room = ClassRoom()
+	room.code = code
+	room.name = name
+	room.center = center
+	room.radius = radius
+	room.save()
+
+if __name__ == '__main__':
+	print "Adding a class.."
+	addClassRoom("1", "My-Class", [1,1], 10)
