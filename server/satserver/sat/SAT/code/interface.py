5241c32aa75ea11a75898fbd6cda1c8963eb4448
Sriram Rathinavelu <sriramrathinavelu@gmail.com>
2015-12-14 18:25:22 -0800

    interface

---
+++ server/satserver/sat/SAT/code/interface.py
@@ -0,0 +1,19 @@
+from utils import *
+from core import *
+import os
+
+def training():
+	gender = 'M'
+	trainUBM(getUBMSourceFolder(gender), gender)
+	updateUnlabeledData(getUBMSourceFolder(gender), gender)
+	gender = 'F'
+	trainUBM(getUBMSourceFolder(gender), gender)
+	updateUnlabeledData(getUBMSourceFolder(gender), gender)
+
+def register(fileName, email, gender):
+	addTrainingSample(fileName, email, gender)
+
+def testing(fileName, email, gender):
+	testFolder = '../data/SPH'
+	convertToSPH (fileName, os.path.join(testFolder, email + '.sph'))
+	test(testFolder, email + '.sph', email, getUBMSourceFolder(gender), gender)
